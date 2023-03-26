b3d492fcb66cfee291ee772094287fb355537eef
Aiden Vigue <aiden@vigue.me>
2023-03-25 21:43:41 -0500

    configure tasks

---
+++ main/constants.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define WORK_JOB_SHOW_SPRITE 1
+
+/* Logger tags */
+#define PROV_TAG "[smx/provisioner]" 
+#define BOOT_TAG "[smx/boot]" 
+#define MATRIX_TAG "[smx/display]" 
+#define MQTT_TAG "[smx/mqtt]" 
+#define WIFI_TAG "[smx/wifi]" 
+#define WORKER_TAG "[smx/worker]" 
+
+/* Inter-task communication values */
+#define MATRIX_TASK_NOT_READY 1
+#define MATRIX_TASK_READY 2
+
+#endif
